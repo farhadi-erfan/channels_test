6b470f3d16caf7159bd0881fb59768c2302b7c6d
Erfan Farhadi <farhadi.erfan76@gmail.com>
2022-07-28 17:42:19 +0300

    deployment try out

--- channels_test/asgi.py
+++ channels_test/asgi.py
@@ -9,18 +9,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 import django
-from channels.auth import AuthMiddlewareStack
-from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 import chat.routing
-from chat.middlewares import JWTTokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channels_test.settings')
 
 django.setup()
+
+from channels.auth import AuthMiddlewareStack
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
